5b84d408bc4d85d0d61deaa1c62e87a47336a13a
Colin Binnie <50568187+Solidus-25@users.noreply.github.com>
2023-05-16 14:04:01 -0400

    convert project to typescript

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+// import logo from './logo.svg';
+import './App.css';
+import { Routes, Route } from "react-router-dom";
+import Main from './components/main';
+import React from 'react';
+
+function App() {
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+
+// root.render(element);
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="*" element={<Main />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
