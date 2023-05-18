2564a2af6a5c33682bf0f8cfd86621302fdd2307
Colin Binnie <50568187+Solidus-25@users.noreply.github.com>
2023-05-18 16:27:42 -0400

    fts update

--- src/pages/fts/fts.tsx
+++ src/pages/fts/fts.tsx
@@ -4,26 +4,28 @@ import './fts.scss';
 class FTSPage extends React.Component {
 
     public testObjects = [
-        { booth: "105", inventoryCode: "0001", description: "test", quantity: 1, price: 4.00, discount: 0 },
-        { booth: "221", inventoryCode: "A400", description: "test", quantity: 1, price: 35.00, discount: 0 },
-        { booth: "106", inventoryCode: "112Q", description: "test", quantity: 1, price: 50.00, discount: 0 },
-        { booth: "323", inventoryCode: "34P", description: "test", quantity: 1, price: 15.00, discount: 0 },
+        { booth: "105", inventoryCode: "0001", description: "test", quantity: 1, price: 499.00, discount: 0 },
+        { booth: "221", inventoryCode: "A400", description: "test", quantity: 1, price: 335.00, discount: 0 },
+        { booth: "106", inventoryCode: "112Q", description: "test", quantity: 1, price: 520.00, discount: 0 },
+        { booth: "323", inventoryCode: "34P", description: "test", quantity: 1, price: 1125.00, discount: 0 },
     ]
 
     private tableItems = new Array<React.JSX.Element>();
 
     private renderTableList(): React.JSX.Element[] {
 
+        this.tableItems = new Array<React.JSX.Element>();
+
         for (let item of this.testObjects) {
             this.tableItems.push(
-                <div className='fts-outline fts-table-item'>
+                <div className='fts-table-item'>
                     <input type='number' data-name="booth" defaultValue={item.booth} />
                     <input data-name="inventoryCode" defaultValue={item.inventoryCode} />
                     <input data-name="description" defaultValue={item.description} />
                     <input type='number' data-name="quantity" defaultValue={item.quantity} />
-                    <input type='number' data-name="price" defaultValue={item.price.toFixed(2)} />
-                    <input data-name='discount' defaultValue={item.discount} />
-                    <input tabIndex={-1} readOnly data-name='total' className='fts-label' defaultValue={(item.price - (item.price * item.discount) * item.quantity).toFixed(2)}/>
+                    <input type='number' step='0.05' data-name="price" defaultValue={item.price.toFixed(2)} />
+                    <input type='number' min="0" max="100" data-name='discount' defaultValue={item.discount} />
+                    <input tabIndex={-1} readOnly data-name='total'  defaultValue={(item.price - (item.price * item.discount) * item.quantity).toFixed(2)}/>
                 </div>);
         }
 
@@ -60,16 +62,18 @@ class FTSPage extends React.Component {
 
                 <div className='fts-outline fts-table-container'>
                     <div className='fts-outline fts-table-header'>
-                        <label data-name='booth' className='fts-label'>Booth #</label>
-                        <label data-name='inventoryCode' className='fts-label'>Inv. #</label>
-                        <label data-name='description' className='fts-label'>Description</label>
-                        <label data-name='quantity' className='fts-label'>Qty.</label>
-                        <label data-name='price' className='fts-label'>Price</label>
-                        <label data-name='discount' className='fts-label'>Pct.</label>
-                        <label data-name='total' className='fts-label'>Total</label>
+                        <label data-name='booth'>Booth #</label>
+                        <label data-name='inventoryCode'>Inv. #</label>
+                        <label data-name='description'>Description</label>
+                        <label data-name='quantity'>Qty.</label>
+                        <label data-name='price'>Price</label>
+                        <label data-name='discount'>Pct.</label>
+                        <label data-name='total'>Total</label>
                     </div>
 
-                    {this.renderTableList()}
+                    <div>
+                        {this.renderTableList()}
+                    </div>
 
                     <button onClick={this.addTableItem} className='fts-button'>+ New</button>
 
@@ -77,12 +81,10 @@ class FTSPage extends React.Component {
                 <div className='fts-side-panel-container'>
                         <div>
                             <label>Discount:</label>
-                            <div>
-                                <input maxLength={2} defaultValue={0}/>
-                            </div>
+                            <span>
+                                <input placeholder='0' maxLength={2} defaultValue={0}/>
+                            </span>
                         </div>
-
-                        <span></span>
 
                         <div>
                             <label>Sub-Total:</label>
@@ -97,13 +99,15 @@ class FTSPage extends React.Component {
                             <label>${(this.getSubtotal() + this.getSalesTax()).toFixed(2)}</label>
                         </div>
 
-                        <div>
+                        <div className="fts-select">
                             <label>Payment Method:</label>
-                            <select>
-                                <option>Debit</option>
-                                <option>Credit</option>
-                                <option>Cash</option>
-                            </select>
+                            <span>
+                                <select>
+                                    <option>Debit</option>
+                                    <option>Credit</option>
+                                    <option>Cash</option>
+                                </select>
+                            </span>
                         </div>
 
                     <button className='fts-button'>Complete Transaction</button>
