0fa1b0fb4dd2d5ae0b0b2768c150b0e3ea2ea209
unknown <corvair25@hotmail.com>
2025-01-08 23:23:24 -0500

    re-added fts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,8 @@
 import Image from "next/image";
 
+import Link from 'next/link'
+ 
+
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
@@ -96,6 +99,7 @@ export default function Home() {
           Go to nextjs.org →
         </a>
       </footer>
+      <Link href="/fts">FTS</Link>
     </div>
   );
 }
