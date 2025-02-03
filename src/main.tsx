26db008ddf910c4afdf7615ff2b9f486adfd5483
unknown <corvair25@hotmail.com>
2025-02-03 16:41:57 -0500

    Recreated with vite

---
+++ src/main.tsx
@@ -0,0 +1,28 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import { BrowserRouter, Route, Routes } from 'react-router';
+import './index.css'
+import App from './App.tsx'
+import FTSPage from './fts/fts.tsx';
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <BrowserRouter>
+    <div className="testing-field-nav">
+      <div>
+        <a href='/'>react-testing-field</a>
+      </div>
+      <div>
+        <a href='/fts'>FTS</a>
+      </div>
+    </div>
+    <div className="testing-field-content">
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/fts" element={<FTSPage />} />
+      </Routes>
+    </div>
+    
+    </BrowserRouter>
+  </StrictMode>
+)
